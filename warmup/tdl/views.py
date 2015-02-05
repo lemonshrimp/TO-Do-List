d3d193d53ed645be161b6968b7880e910b2799c6
Arnold Yichu Lin <ylin87@dutch.mathcs.emory.edu>
2015-02-05 02:53:00 -0500

    CSS

--- warmup/tdl/views.py
+++ warmup/tdl/views.py
@@ -1,9 +1,12 @@
 from models import To_do
 from django.shortcuts import render_to_response
+import os
+
+STATIC_URL = os.path.dirname(os.path.dirname('__init__'))
  
 def index(request): #Define our function, accept a request
  
     items = To_do.objects.all() #ORM queries the database for all of the to-do entries.
  
-    return render_to_response('index.html', {'items': items}) #Responds with passing the object items (contains info from the DB) to the template index.html
+    return render_to_response('index.html', {'items': items, 'STATIC_URL': os.path.join(STATIC_URL,'/static/')}) #Responds with passing the object items (contains info from the DB) to the template index.html
 
--- warmup/warmup/settings.py
+++ warmup/warmup/settings.py
@@ -10,8 +10,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.realpath(os.path.dirname('setting.py'))
+BASE_DIR = os.path.dirname(os.path.dirname('__file__'))
 
+PROJ_PATH = os.path.dirname(os.path.abspath('__file__'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -83,9 +84,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = {
+STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, "/static/"),
-}
+)
 
 STATIC_ROOT = os.path.join(BASE_DIR, "/tdl/static/")
 
