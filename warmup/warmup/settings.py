7142bad6e8f16899a780c51f7d100383a33a334d
Arnold Yichu Lin <ylin87@dutch.mathcs.emory.edu>
2015-02-05 00:27:16 -0500

    modified Feb5th

--- warmup/warmup/settings.py
+++ warmup/warmup/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+#    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'warmup.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '/warmup/database/database.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '/warmup/database/database.sqlite3'),
     }
 }
 
